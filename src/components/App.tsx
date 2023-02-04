b9ff15f5ad3e65e2cb7cd9def33cd0c5783b3b4f
Garnik Simonyan <garnik.simonyan@stoiclane.com>
2023-02-04 11:54:19 +0400

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+import s from '../styles/AppStyles';
+
+const App = () => {
+  return (
+    <View style={s.container}>
+      <Text>Hello RN</Text>
+    </View>
+  );
+};
+
+export default App;
